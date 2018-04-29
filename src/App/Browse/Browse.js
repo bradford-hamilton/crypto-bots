a9d3579d18d75829964d0495d925c32a86c864c0
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-04-29 17:49:33 -0600

    [feature] browse bots

--- src/App/Browse/Browse.js
+++ src/App/Browse/Browse.js
@@ -4,26 +4,52 @@ import {
   Row,
   Col,
   PageHeader,
+  Image,
 } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 import getWeb3 from '../../utils/getWeb3';
 import NavBar from '../NavBar/NavBar';
+import BotCore from '../../../build/contracts/BotCore.json';
 import './Browse.css';
 
 class Browse extends Component {
   constructor(props) {
     super(props);
-    this.state = { web3: null };
+    this.state = { web3: null, botsForSale: [] };
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
     getWeb3
-      .then(results => this.setState({ web3: results.web3 }))
+      .then(results => {
+        this.setState({ web3: results.web3 });
+        this.listBots();
+      })
       .catch(() => console.log('Error finding web3.'));
   }
 
+  async listBots() {
+    const contract = require('truffle-contract');
+    const botCore = contract(BotCore);
+
+    botCore.setProvider(this.state.web3.currentProvider);
+
+    const botCoreInstance = await botCore.deployed();
+    const botIds = await botCoreInstance.listBotIds();
+    const botPrices = await botCoreInstance.listBotPrices();
+    const botOwners = await botCoreInstance.listBotOwners();
+    const botsForSale = botIds.map((botId, i) => ({
+      id: botId.toNumber(),
+      price: this.state.web3.fromWei(botPrices[i].toNumber(), 'ether'),
+      owner: botOwners[i],
+    }));
+
+    this.setState({ botsForSale });
+  }
+
+
   render() {
+    if (!this.state.web3) return <h1>Loading...</h1>;
+
     return (
       <div className="browse">
         <NavBar />
@@ -35,8 +61,17 @@ class Browse extends Component {
               </PageHeader>
             </Col>
           </Row>
-          <Row>
-            <p>things go here</p>
+          <Row className="bot-row">
+            {this.state.botsForSale.map(bot => (
+              <Col xs={12} md={4} key={bot.id} className="bot-card-wrapper">
+                <div className="bot-card">
+                  <Image src={`https://robohash.org/${bot.id}`} rounded />
+                  <p className="bot-id">Bot #{bot.id}</p>
+                  <p className="price">Price: {bot.price} ether</p>
+                  <p className="address">Owner Address: {bot.owner}</p>
+                </div>
+              </Col>
+            ))}
           </Row>
         </Grid>
       </div>
