9cfa10f1edaea0a13e7f0933530437992d3bca95
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-06-10 20:53:53 -0600

    small format update

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -10,9 +10,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
     if (typeof web3 !== 'undefined') {
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider);
-
       results = { web3: web3 };
-
       console.log('Injected web3 detected.');
 
       resolve(results);
@@ -22,9 +20,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
       var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
 
       web3 = new Web3(provider);
-
       results = { web3: web3 };
-
       console.log('No web3 instance injected, using Local web3.');
 
       resolve(results);
